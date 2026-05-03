1d9d4bd0e83a9202468c19680578507102926305
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-05-03 17:47:21 +0800

    Update build_script.sh

--- build_script.sh
+++ build_script.sh
@@ -38,7 +38,7 @@ cd vendor/evolution-priv/keys
 ./keys.sh
 cd -
 
-grep -q "vendor/evolution-priv/keys/keys.mk" device/xiaomi/platina/BoardConfig.mk || sed -i '$ a include vendor/evolution-priv/keys/keys.mk' device/xiaomi/platina/BoardConfig.mk
+grep -q "vendor/evolution-priv/keys/keys.mk" device/xiaomi/platina/BoardConfig.mk || sed -i '$ a -include vendor/evolution-priv/keys/keys.mk' device/xiaomi/platina/aosp_platina.mk
 sed -i 's/PRODUCT_CERTIFICATE_OVERRIDES/PRODUCT_PACKAGE_NAME_OVERRIDES/g' vendor/evolution-priv/keys/keys.mk
 tail -5 device/xiaomi/platina/BoardConfig.mk
 

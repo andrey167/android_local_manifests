19944ecebbd14b819a3fe4fe7d7a2d351702b016
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-05-03 17:48:13 +0800

    Update build_script.sh

--- build_script.sh
+++ build_script.sh
@@ -39,8 +39,8 @@ cd vendor/evolution-priv/keys
 cd -
 
 grep -q "vendor/evolution-priv/keys/keys.mk" device/xiaomi/platina/BoardConfig.mk || sed -i '$ a -include vendor/evolution-priv/keys/keys.mk' device/xiaomi/platina/aosp_platina.mk
-sed -i 's/PRODUCT_CERTIFICATE_OVERRIDES/PRODUCT_PACKAGE_NAME_OVERRIDES/g' vendor/evolution-priv/keys/keys.mk
-tail -5 device/xiaomi/platina/BoardConfig.mk
+#sed -i 's/PRODUCT_CERTIFICATE_OVERRIDES/PRODUCT_PACKAGE_NAME_OVERRIDES/g' vendor/evolution-priv/keys/keys.mk
+tail -5 device/xiaomi/platina/aosp_platina.mk
 
 echo "==> Lunching target..."
 lunch aosp_platina-bp1a-user
